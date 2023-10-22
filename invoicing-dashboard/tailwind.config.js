2fb276d76e541037cd0261bcb41930d19923257c
PemulungEmperor <yuliuslebelamak@yahoo.com>
2023-10-23 00:29:31 +0700

    add more weight for font montserrat

--- invoicing-dashboard/tailwind.config.js
+++ invoicing-dashboard/tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", "./node_modules/flowbite/**/*.js"],
   theme: {
     fontFamily: {
       Montserrat: ["Montserrat"],
     },
+    fontWeight: {
+      light: 300,
+      normal: 400,
+      medium: 500,
+      mediumBold: 600,
+      bold: 700,
+    },
     extend: {},
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin")],
 };
